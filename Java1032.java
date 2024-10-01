b16328ca84551cf6009529eca82b0f3e4dbf2dbf
Kim Jin Gun <steadysince240925@gmail.com>
2024-10-02 01:08:37 +0900

    1032 번 문제 해결 Java로 해결

---
+++ Java1032.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 1032{
+    public static void main(String args[]){
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(System.in));
+            bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+            int stringNumber = Integer.parseInt(br.readLine());
+            String targetString = null;
+            String[] targetCharArr = null;
+            String[] answerArr = null;
+            for(int idx = 1; idx <= stringNumber; idx++){
+                targetString = br.readLine();
+                targetCharArr = targetString.split("");
+
+                for(int splitedIdx = 0; splitedIdx < targetCharArr.length; splitedIdx++){
+                    if(idx == 1){
+                        answerArr = targetCharArr;
+                    }else{
+                        if(answerArr[splitedIdx].equals(targetCharArr[splitedIdx]) == false){
+                            answerArr[splitedIdx] = "?";
+                        }
+                    }
+                }
+            }
+
+            bw.write(String.join("",answerArr));
+            bw.flush();
+            
+        } catch (Exception e) {
+            
+        } finally{
+            try {
+                br.close();
+                bw.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
