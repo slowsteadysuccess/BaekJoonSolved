68bc8d2822d08bfc1c63736824ef7156c6d2ef36
Kim Jin Gun <steadysince240925@gmail.com>
2024-10-03 22:02:21 +0900

    1008 번 문제 해결

---
+++ 1008.js
@@ -0,0 +1,4 @@
+let numbers = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./answerSheet.txt").toString().trim().split(" ");
+numbers[0] = parseInt(numbers[0]);
+numbers[1] = parseInt(numbers[1]);
+console.log(numbers[0] / numbers[1]);
